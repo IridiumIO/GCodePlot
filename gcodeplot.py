2ec18174923bfc10b5dcd7bd3b940ae4a7a25af7
IridiumIO <iridium.io@outlook.com>
2023-12-31 15:14:11 +1000

    Add option to ignore locked SVG elements (for Inkscape SVGs only)

--- gcodeplot.py
+++ gcodeplot.py
@@ -714,11 +714,26 @@ def remove_hidden_SVGElements(svgTree):
                 remove_hidden_SVGElements(prop)
 
 
+def remove_locked_InkscapeSVGElements(svgTree:ET.Element):
+
+    prop_parents = svgTree.findall('*' + '/..')
+    for parent in prop_parents:
+        for prop in parent.findall('*'):
+
+            type = prop.get('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}insensitive', None)
+        
+            if type is not None:
+                parent.remove(prop)     
+            else:
+                remove_locked_InkscapeSVGElements(prop)
+
+
 def generate_pen_data(svgTree, data, args, shader:Shader):
     penData = {}
     
     if svgTree is not None:
-        remove_hidden_SVGElements(svgTree) if args.ignore_hidden else svgTree
+        if args.ignore_hidden: remove_hidden_SVGElements(svgTree)
+        if args.ignore_locked: remove_locked_InkscapeSVGElements(svgTree)
         penData = parseSVG(svgTree, tolerance=args.tolerance, shader=shader, strokeAll=args.stroke_all, pens=args.pens, extractColor=args.extract_color if args.boolean_extract_color else None)
     else:
         penData = parseHPGL(data, dpi=args.input_dpi)
@@ -817,6 +832,8 @@ def parse_arguments(argparser:cArgumentParser):
     argparser.add_argument('-L', '--stroke-all', action=argparse.BooleanOptionalAction, default=False, help='stroke even regions specified by SVG to have no stroke')
     argparser.add_argument('-e', '--direction', metavar='ANGLE', default=None, type=lambda value: None if value.lower() == 'none' else float(value), help='for slanted pens: prefer to draw in given direction (degrees; 0=positive x, 90=positive y, none=no preferred direction) [default none]')
     argparser.add_argument('--ignore-hidden', action=argparse.BooleanOptionalAction, default=True, help='ignore hidden SVG elements')
+    argparser.add_argument('--ignore-locked', action=argparse.BooleanOptionalAction, default=True, help='ignore locked SVG elements (for Inkscape SVG only)')
+    
     
     argparser.add_argument('-o', '--optimization-time', metavar='T', default=60, type=int, help='max time to spend optimizing (seconds; set to 0 to turn off optimization) [default 60]')
     argparser.add_argument('-d', '--sort', action=argparse.BooleanOptionalAction, default=False, help='sort paths from inside to outside for cutting [default off]')
